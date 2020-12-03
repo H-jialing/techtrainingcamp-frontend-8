e02dabdcc62e432e11f9052516a5428341b9cb0c
jacqueline <706632932@qq.com>
2020-12-03 16:18:11 +0800

    hjl-fix: 解决退出房间路由问题

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ import NotFound from '../pages/NotFound.vue'
 
 Vue.use(Router)
 
+const originalPush = Router.prototype.push
+
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 export default new Router({
   // mode: 'history',
   routes: [
